24429f97d420a54838fa82fdd5b58eb0dc4aee41
Nicola Zilio <nicolazilio@gmail.com>
2018-06-01 10:09:31 +0200

    Remove action to add lab users to GoogleDocs from user_management admin file

--- user_management/admin.py
+++ user_management/admin.py
@@ -9,27 +9,6 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from django.contrib import messages
-
-#################################################
-#        ADDED FUNCTIONALITIES IMPORTS          #
-#################################################
-
-import os
-import sys
-
-# Google Sheets API to add new orders to Order Master List
-import pygsheets
-
-#################################################
-#                OTHER IMPORTS                  #
-#################################################
-
-from django_project.private_settings import LAB_MEMBERS_SHEET_ID
-
-# Switch from default ASCII to utf-8 encoding, needed for the Google Docs stuff to work
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
 #################################################
 #       EXTENDED LAB MEMEBER USER INLINES       #
@@ -64,26 +43,3 @@ class LabUserAdmin(BaseUserAdmin):
         pretty_name = self.first_name[0].upper() + '. ' + self.last_name.title()
         return pretty_name
     User.__unicode__ = user_pretty_name
-
-    actions = ['update_lab_users_google_sheet']
-
-    def update_lab_users_google_sheet (self, request, queryset):
-        """ Update active user list sheet on GoogleDocs """
-        try:
-            # Log in to GoogleDocs
-            base_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
-            gc = pygsheets.authorize(service_file=base_path + "/beyond_django/gdrive_access_credentials.json", no_cache=True)
-            # Open user list Google sheet
-            spreadsheet = gc.open_by_key(LAB_MEMBERS_SHEET_ID)
-            worksheet = spreadsheet.worksheet('title', 'Users')
-            # Get list of active users 
-            users = [[user.first_name, user.last_name, user.email, user.labuser.abbreviation_code] \
-                    for user in User.objects.filter(is_active=True).exclude(id__in=[1,6,20]).exclude(groups__name='Guest').order_by('last_name')]
-            # Update user list Google sheet
-            worksheet.clear(start=(2,1))
-            worksheet.update_cells(crange=(2,1), values=users, extend=True)
-            messages.success(request, 'The user list on GoogleDocs was updated successfully')
-        except Exception, err:
-            messages.error(request, 'The user list on GoogleDocs could not be updated. Error: ' + str(err))
-
-    update_lab_users_google_sheet.short_description = "Update lab user list on GoogleDocs"

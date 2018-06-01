7002009d8959e7f89e20aa6b67ba98ce74f7d41b
Nicola Zilio <nicolazilio@gmail.com>
2018-06-01 09:55:02 +0200

    Add action to export recipes/methods as .docx

--- pages/admin.py
+++ pages/admin.py
@@ -25,6 +25,46 @@ from simple_history.admin import SimpleHistoryAdmin
 # Import/Export functionalities from django-import-export
 from import_export.admin import ExportActionModelAdmin
 
+def export_as_docx(modeladmin, request, queryset):
+    """ Admin adction to export Protocol or Recipe as
+    a .docx file
+    """
+    import pypandoc
+    from django_project.settings import MEDIA_ROOT
+    from django.http import HttpResponse
+    import os
+    import string
+    from random import choice
+    from bs4 import BeautifulSoup
+    from django.contrib import messages
+    import time
+
+    try:
+        allchar = string.ascii_letters + string.punctuation + string.digits
+
+        input_html_text = ''
+        for item in queryset:
+            input_html_text += '<h1>' + item.title + '</h1>' + item.content
+        
+        soup = BeautifulSoup(input_html_text, "html.parser")
+        for img in soup.findAll('img'):
+            img.attrs["src"] = 'http://localhost:8080' + img.attrs["src"]
+
+        file_name = MEDIA_ROOT + "temp/" + "".join(choice(allchar) for x in range(20))
+        pypandoc.convert_text(soup, 'docx', format='html', outputfile=file_name,  extra_args=['-V', 'papersize:a4'])
+        
+        with open (file_name, 'r') as download_file:
+            response = HttpResponse(download_file, content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+            response['Content-Disposition'] = 'attachment; filename=pages_' + time.strftime("%Y%m%d") + "_" + time.strftime("%H%M%S") + '.docx'
+        os.remove(file_name)
+    except Exception, err:
+        messages.error(request, "Could not export record. Error: " + str(err))
+    if 'response' not in locals():
+        response = ''
+    return response
+
+export_as_docx.short_description = "Export selected as .docx"
+
 #################################################
 #               PROTOCOL PAGES                  #
 #################################################
@@ -35,6 +75,7 @@ class ProtocolPage(ExportActionModelAdmin, DjangoQLSearchMixin, SimpleHistoryAdm
     fields = ('title', 'content', 'tags')
     list_per_page = 25
     formfield_overrides = {models.CharField: {'widget': TextInput(attrs={'size':'93'})},}
+    actions = [export_as_docx]
 
     def save_model(self, request, obj, form, change): 
         '''Override default save_model to assign object to user who created it'''
@@ -51,13 +92,13 @@ class ProtocolPage(ExportActionModelAdmin, DjangoQLSearchMixin, SimpleHistoryAdm
         return ', '.join(instance.tags.values_list('name',flat=True))
     get_tags.short_description = 'Tags'
 
-
 class RecipePage(ExportActionModelAdmin, DjangoQLSearchMixin, SimpleHistoryAdmin, admin.ModelAdmin):
     list_display = ('title', 'get_tags')
     list_display_links = ('title', )
     fields = ('title', 'content', 'tags')
     list_per_page = 25
     formfield_overrides = {models.CharField: {'widget': TextInput(attrs={'size':'93'})},}
+    actions = [export_as_docx]
 
     def save_model(self, request, obj, form, change): 
         '''Override default save_model to assign object to user who created it'''

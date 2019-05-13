b765ce7e917cd904c95d55e47acdca8506d03914
Nicola Zilio <nicolazilio@gmail.com>
2019-05-13 12:17:24 +0200

    Djangonize sending weekly reminder email

--- beyond_django/weekly_digest.py
+++ beyond_django/weekly_digest.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 
-import smtplib
 import inspect
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from django_project.private_settings import DJANGO_PRIVATE_DATA
 
-def send_email(text):
-    user = None
-    pwd = None
-    FROM = "system@imbc2.imb.uni-mainz.de"
-    TO = ['H.Ulrich@imb-mainz.de'] #must be a list
-    SUBJECT = "Ulrich lab intranet weekly notification"
-    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s""" % (FROM, ", ".join(TO), SUBJECT, text)
-    server = smtplib.SMTP("localhost", 25)
-    server.sendmail(FROM, TO, message.encode('utf8'))
-    server.close()
+H_ULRICH_USER = User.objects.get(username='hprofdru')
 
-EMAIL_MESSAGE_TXT = inspect.cleandoc("""Dear Helle,
+EMAIL_MESSAGE_TXT = inspect.cleandoc("""Dear {},
 
 Please visit https://imbc2.imb.uni-mainz.de:8443/approval_summary/ to check for new or modified records that need to be approved.
 
 Best wishes,
 The Ulrich lab intranet
-""")
-send_email(EMAIL_MESSAGE_TXT)
+""".format(H_ULRICH_USER.first_name))
+
+send_mail(
+    "Ulrich lab intranet weekly notification",
+    EMAIL_MESSAGE_TXT,
+    DJANGO_PRIVATE_DATA["server_email_address"],
+    [H_ULRICH_USER.email],
+)

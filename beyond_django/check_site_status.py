e13e38bfae49318324848f7df780841bdf7b4c4c
Nicola Zilio <nicolazilio@gmail.com>
2019-05-27 09:12:44 +0200

    Changes after porting to new server

--- beyond_django/check_site_status.py
+++ beyond_django/check_site_status.py
@@ -12,8 +12,7 @@ new_status_code = 200 # Set the intial status to that of a working website
 
 while True:
     old_status_code = new_status_code
-    # new_status_code = requests.get('https://imbc2.imb.uni-mainz.de:8443/', verify=False).status_code
-    new_status_code = requests.get("https://127.0.0.1:8443/", verify=False).status_code
+    new_status_code = requests.get("https://127.0.0.1/", verify=False).status_code
     if new_status_code != 200 and old_status_code == 200:
         mail_admins("The Ulrich intranet is down", "The Ulrich intranet is down", fail_silently=True)
         try:

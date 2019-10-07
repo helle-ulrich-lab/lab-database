cfe1c00175aeb5d60e0218e8d859cda4d46dfc69
Nicola Zilio <nicolazilio@gmail.com>
2019-10-07 11:49:10 +0200

    Add sh file for checking intranet status

---
+++ beyond_django/check_site_status.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+/home/nzilio/ulrich_lab_intranet/bin/python /home/nzilio/ulrich_lab_intranet/django_project/manage.py shell < /home/nzilio/ulrich_lab_intranet/django_project/beyond_django/check_site_status.py

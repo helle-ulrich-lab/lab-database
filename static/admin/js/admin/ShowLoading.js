410867dfc1393a3e2ea433c856bd161e62db85dc
Nicola Zilio <nicolazilio@gmail.com>
2019-11-03 11:49:46 +0100

    Change loading animation

--- static/admin/js/admin/ShowLoading.js
+++ static/admin/js/admin/ShowLoading.js
@@ -2,8 +2,8 @@ function ShowLoading(e) {
     var div = document.createElement('div');
     var img = document.createElement('img');
     img.style.cssText = 'display:block, height: 40%; width:auto;'
-    img.src = '/static/admin/img/helle.gif'; //'/static/admin/img/spinner.svg';
-    div.innerHTML = "</br>Wart' mal 'n Moment! ...</br></br>";
+    img.src = '/static/admin/img/loader.gif'; //'/static/admin/img/spinner.svg';
+    div.innerHTML = "</br>Please wait...</br></br>";
     div.style.cssText = 'margin: 0; height: 12vw; width:15vw; position: fixed; \
      top: 50%; left:50%; z-index: 5000; transform: translatey(-50%); \
      box-shadow: 0 0 0 100vmax rgba(0,0,0,.3); background-color: white; \

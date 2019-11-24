e2903e8c54bef78498858a7be452a07a874446ad
Nicola Zilio <nicolazilio@gmail.com>
2019-11-24 06:39:55 +0100

    Send weekly reminder email only there are records to be approved

--- beyond_django/weekly_tasks.py
+++ beyond_django/weekly_tasks.py
@@ -1,30 +1,32 @@
-# -*- coding: utf-8 -*-
-
 import inspect
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
 from django_project.private_settings import ALLOWED_HOSTS
 from django_project.private_settings import SITE_TITLE
+from django_project.private_settings import SERVER_EMAIL_ADDRESS
 from django.urls import reverse
+from record_approval.models import RecordToBeApproved
 
-PI_USER = User.objects.get(labuser__is_principal_investigator=True)
+if RecordToBeApproved.objects.all().exists(): # Check if there are records to be be approved at all
 
-RECORD_APPROVAL_URL = reverse("admin:record_approval_recordtobeapproved_changelist")
+    PI_USER = User.objects.get(labuser__is_principal_investigator=True)
 
-EMAIL_MESSAGE_TXT = inspect.cleandoc("""Dear {},
+    RECORD_APPROVAL_URL = reverse("admin:record_approval_recordtobeapproved_changelist")
 
-Please visit https://{}{} to check for new or modified records that need to be approved.
+    EMAIL_MESSAGE_TXT = inspect.cleandoc("""Dear {},
 
-Best wishes,
-The {}
-""".format(PI_USER.first_name, ALLOWED_HOSTS[0], RECORD_APPROVAL_URL, SITE_TITLE))
+    Please visit https://{}{} to check for new or modified records that need to be approved.
 
-send_mail(
-    "{} weekly notification".format(SITE_TITLE),
-    EMAIL_MESSAGE_TXT,
-    DJANGO_PRIVATE_DATA["server_email_address"],
-    [PI_USER.email],
-)
+    Best wishes,
+    The {}
+    """.format(PI_USER.first_name, ALLOWED_HOSTS[0], RECORD_APPROVAL_URL, SITE_TITLE))
+
+    send_mail(
+        "{} weekly notification".format(SITE_TITLE),
+        EMAIL_MESSAGE_TXT,
+        SERVER_EMAIL_ADDRESS,
+        [PI_USER.email],
+    )
 
 # Delete all completed tasks
 

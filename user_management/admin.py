e850470bc22934e34702578512527b5bf8708df4
Nicola Zilio <nicolazilio@gmail.com>
2019-11-27 17:23:34 +0100

    Restrict which groups a user can see in UserAdmin

--- user_management/admin.py
+++ user_management/admin.py
@@ -23,6 +23,7 @@ class LabUserInline(admin.StackedInline):
 class LabUserAdmin(BaseUserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'get_user_groups')
     inlines= (LabUserInline,)
+    critical_groups = None
     
     def save_model(self, request, obj, form, change):
         
@@ -33,7 +34,16 @@ class LabUserAdmin(BaseUserAdmin):
             obj.save()
             LabUser.objects.create(user=obj)
         else:
-            obj.save()
+            
+            # If somebody tries to save a user for which it cannot see some of its groups,
+            # save these groups to critical_groups and add them back to the user in save_related
+            if not request.user.is_superuser or not request.user.labuser.is_principal_investigator:
+                old_user = User.objects.get(id=obj.pk)
+                critical_groups = old_user.groups.exclude(name__in=['Guest', 'Regular lab member', 'Order manager', 'Lab manager'])
+                self.critical_groups = list(critical_groups)
+                obj.save()
+            else:
+                obj.save()
 
         # If is_principal_investigator is True check whether a principal_investigator already exists
         # and if so set the field to False
@@ -42,12 +52,24 @@ class LabUserAdmin(BaseUserAdmin):
                 obj.labuser.is_principal_investigator = False
                 obj.save()
 
+    def save_related(self, request, form, formsets, change):
+
+        '''If somebody tries to save a user for which it cannot see some of its groups,
+        get these groups from critical_groups and add them back to the user'''
+        
+        super(LabUserAdmin, self).save_related(request, form, formsets, change)
+
+        if self.critical_groups:
+            obj = User.objects.get(pk=form.instance.id)
+            for g in self.critical_groups:
+                obj.groups.add(g)
+
     def change_view(self,request,object_id,extra_context=None):
         '''Override default change_view to show only desired fields'''
         
         extra_context = extra_context or {}
 
-        if request.user.is_superuser:
+        if request.user.is_superuser or request.user.labuser.is_principal_investigator:
             self.fieldsets = (
                 (None, {'fields': ('username', 'password')}),
                 (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
@@ -67,7 +89,7 @@ class LabUserAdmin(BaseUserAdmin):
         return super(LabUserAdmin,self).change_view(request,object_id, extra_context=extra_context)
 
     def get_formsets_with_inlines(self, request, obj=None):
-        """Show is_principal_inevstigator inline only for superusers"""
+        """Show is_principal_investigator inline only for superusers"""
         
         if request.user.is_superuser:
             for inline in self.get_inline_instances(request, obj):
@@ -100,3 +122,15 @@ class LabUserAdmin(BaseUserAdmin):
         else:
             return self.username
     User.__str__ = user_pretty_name
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        '''Show important groups only to superusers and principal investigators'''
+
+        from django.contrib.auth.models import Group
+
+        if db_field.name == "groups":
+            if request.user.is_superuser or request.user.labuser.is_principal_investigator:
+                kwargs["queryset"] =  Group.objects.all()
+            else:
+                kwargs["queryset"] = Group.objects.filter(name__in=['Guest', 'Regular lab member', 'Order manager', 'Lab manager'])
+        return super(LabUserAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)

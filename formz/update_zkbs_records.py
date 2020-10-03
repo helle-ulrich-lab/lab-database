48966e6a8051b72d9854c131e295e2b7b53d5d5f
Nicola Zilio <nicolazilio@gmail.com>
2020-10-03 11:38:43 +0200

    Add ability to update ZKBS record tables

--- formz/update_zkbs_records.py
+++ formz/update_zkbs_records.py
@@ -31,15 +31,29 @@ def update_zkbs_celllines(excel_file):
             for row in rows:
                 
                 # If cell line not in database, add it 
-                if not ZkbsCellLine.objects.filter(name__exact=row[0].value.strip()).exists():
+                qs = ZkbsCellLine.objects.filter(name__exact=row[0].value.strip())
+
+                if not qs:
                 
                     ZkbsCellLine.objects.create(name=row[0].value.strip(), 
                                             synonym=row[1].value if row[1].value else "", 
-                                            organism= row[3].value if row[3].value else "", 
+                                            organism=row[3].value if row[3].value else "", 
                                             risk_potential=row[2].value if row[2].value else "",
                                             origin=row[4].value if row[4].value else "",
                                             virus=row[5].value if row[5].value else "",
-                                            genetically_modified= True if row[6].value == 'Ja' else False)
+                                            genetically_modified=True if row[6].value == 'Ja' else False)
+
+                else:
+                    
+                    if len(qs) == 1:
+                        zkbs_cell_line = qs.first()
+                        zkbs_cell_line.synonym = row[1].value if row[1].value else ""
+                        zkbs_cell_line.organism = row[3].value if row[3].value else ""
+                        zkbs_cell_line.risk_potential = row[2].value if row[2].value else ""
+                        zkbs_cell_line.origin = row[4].value if row[4].value else ""
+                        zkbs_cell_line.virus = row[5].value if row[5].value else ""
+                        zkbs_cell_line.genetically_modified = True if row[6].value == 'Ja' else False
+                        zkbs_cell_line.save()
 
         else:
 
@@ -87,14 +101,25 @@ def update_zkbs_plasmids(excel_file):
 
             for row in rows:
                 
-                # If plasmid not in database, add it 
-                if not ZkbsPlasmid.objects.filter(name__exact=row[0].value.strip()).exists():
+                # If plasmid not in database, add it, if not update it
+                qs = ZkbsPlasmid.objects.filter(name__exact=row[0].value.strip())
+                
+                if not qs:
 
                     ZkbsPlasmid.objects.create(name=row[0].value.strip(), 
                                            source=row[2].value if row[2].value else "", 
                                            purpose= row[1].value if row[1].value else "", 
                                            description=row[4].value if row[4].value else "")
-                
+
+                else:
+
+                    if len(qs) == 1:
+                        zkbs_plasmid = qs.first()
+                        zkbs_plasmid.source = row[2].value if row[2].value else ""
+                        zkbs_plasmid.purpose = row[1].value if row[1].value else ""
+                        zkbs_plasmid.description = row[4].value if row[4].value else ""
+                        zkbs_plasmid.save()
+
         else:
 
             error_messages.append("The ZKBS plasmids were not updated because the column titles, in the third row of the file you uploaded, do not match the expected values: "
@@ -142,7 +167,9 @@ def update_zkbs_oncogenes(excel_file):
             for row in rows:
                 
                 # If oncogene not in database, add it
-                if not ZkbsOncogene.objects.filter(name__exact=row[1].value.strip()).exists():
+                qs = ZkbsOncogene.objects.filter(name__exact=row[1].value.strip())
+
+                if not qs:
 
                     ZkbsOncogene.objects.create(name=row[1].value.replace("*", "").strip(), # next time remove asterisk from all names!!!
                                 synonym=row[2].value if row[2].value else "", 
@@ -150,6 +177,17 @@ def update_zkbs_oncogenes(excel_file):
                                 risk_potential=row[4].value if row[4].value else "",
                                 reference=row[5].value if row[5].value else "",
                                 additional_measures=True if row[6].value == 'Ja' else False)
+
+                else:
+
+                    if len(qs) == 1:
+                        zkbs_oncogene = qs.first()
+                        zkbs_oncogene.synonym = row[2].value if row[2].value else ""
+                        zkbs_oncogene.species = row[3].value if row[3].value else ""
+                        zkbs_oncogene.risk_potential = row[4].value if row[4].value else ""
+                        zkbs_oncogene.reference = row[5].value if row[5].value else ""
+                        zkbs_oncogene.additional_measures = True if row[6].value == 'Ja' else False
+                        zkbs_oncogene.save()
             
         else:
 

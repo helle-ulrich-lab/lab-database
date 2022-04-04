a5884f1c2955ca88fb12d17a1bace31cb48573cf
Nicola Zilio <nicolazilio@gmail.com>
2022-04-04 10:05:04 +0200

    Generalize js for order autocomplete

--- static/admin/js/admin/order_management_order_change_form.js
+++ static/admin/js/admin/order_management_order_change_form.js
@@ -1,43 +1,24 @@
 // Order autocomplete
 
-let fillInFields = data => {
-    Object.entries(data).forEach((fieldData) =>{
-        $(`#id_${fieldData[0]}`).val(fieldData[1]);
+$('#id_part_description,#id_supplier_part_no').click(function() {
+    
+    let field_name = $(this).attr('id').replace("id_", "");
+
+    $(`#id_${field_name}`).autocomplete({
+
+        minLength: 3,
+    
+        source: (request, response) => {
+            let ts = Date.now();
+            $.getJSON(`/order_management/order_autocomplete/${field_name}=${request.term.trim().replace('=', '')},${ts}`, data => {
+                response(data);
+            })
+        },
+    
+        select: (e, ui) => {
+            Object.entries(ui.item.data).forEach((fieldData) =>{
+                $(`#id_${fieldData[0]}`).val(fieldData[1]);
+            });
+        }
     });
-}
-
-// Autocomplete for part_description
-
-$('#id_part_description').autocomplete({
-
-    minLength: 3,
-
-    source: (request, response) => {
-        let ts = Date.now();
-        $.getJSON(`/order_management/order_autocomplete/part_description=${request.term.trim().replace('=', '')},${ts}`, data => {
-            response(data);
-        })
-    },
-
-    select: (e, ui) => {
-        fillInFields(ui.item.data);
-    }
-});
-
-// Autocomplete for supplier_part_no
-
-$('#id_supplier_part_no').autocomplete({
-
-    minLength: 3,
-
-    source: (request, response) => {
-        let ts = Date.now();
-        $.getJSON(`/order_management/order_autocomplete/supplier_part_no=${request.term.trim().replace('=', '')},${ts}`, data => {
-            response(data);
-        })
-    },
-
-    select: (e, ui) => {
-        fillInFields(ui.item.data);
-    }
 });

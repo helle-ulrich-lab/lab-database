aae8da3b9116d091298c96d3a895b601eb4df347
Nicola Zilio <nicolazilio@gmail.com>
2022-04-06 18:24:15 +0200

    Even better handling and styling of order autocomplete

--- static/admin/js/admin/order_management_order_change_form.js
+++ static/admin/js/admin/order_management_order_change_form.js
@@ -2,15 +2,22 @@
 
 $('#id_part_description,#id_supplier_part_no').click(function () {
 
-    let field_name = $(this).attr('id').replace("id_", "");
+    let LabelFieldName = $(this).attr('id').replace('id_', '');
+    let firstDataFieldName = LabelFieldName === 'part_description' ? 'supplier_part_no' : 'part_description';
+    let dotifyText = function (text, maxLength) {
+        if (text.length > maxLength) {
+           return text.substring(0, maxLength) + '...';
+        }
+        return text;
+     };
 
-    $(`#id_${field_name}`).autocomplete({
+    $(`#id_${LabelFieldName}`).autocomplete({
 
         minLength: 3,
 
         source: (request, response) => {
-            let ts = Date.now();
-            $.getJSON(`/order_management/order_autocomplete/${field_name}=${request.term.trim().replace('=', '')},${ts}`, data => {
+            let timeStamp = Date.now();
+            $.getJSON(`/order_management/order_autocomplete/${LabelFieldName}=${request.term.trim().replace('=', '')},${timeStamp}`, data => {
                 response(data);
             })
         },
@@ -21,11 +28,11 @@ $('#id_part_description,#id_supplier_part_no').click(function () {
             });
         },
     }).autocomplete("instance")._renderItem = function (ul, item) {
+        let spanStyle = 'color:#417690; margin:0; padding-left:3px; padding-right:3px;';
+        let displaText = [dotifyText(item['label'], 50), item['data'][firstDataFieldName], item['data']['supplier'].replace(" GmbH", "")].join(`<span style='${spanStyle}'>⬧</span>`);
         return $("<li>")
             .attr("data-value", item.value)
-            .append([item['label'], Object.values(item['data'])[0], Object.values(item['data'])[1].replace(" GmbH", "")].join("<span style='color:indianred; margin:0; padding-left:5px; padding-right:5px;'>|</span>"))
+            .append(`<span style='margin:0; padding-right:5px; padding-left:5px;'>${displaText}</span>`)
             .appendTo(ul);
-
     }
 });
-

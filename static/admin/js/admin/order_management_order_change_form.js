c3642344eb651fabdaf3a2cadff63c68c4b62543
Nicola Zilio <nicolazilio@gmail.com>
2022-04-11 09:43:27 +0200

    Better format JS code for autocomplete

--- static/admin/js/admin/order_management_order_change_form.js
+++ static/admin/js/admin/order_management_order_change_form.js
@@ -2,8 +2,9 @@
 
 $('#id_part_description,#id_supplier_part_no').click(function () {
 
-    let LabelFieldName = $(this).attr('id').replace('id_', '');
-    let firstDataFieldName = LabelFieldName === 'part_description' ? 'supplier_part_no' : 'part_description';
+    let labelFieldName = $(this).attr('id').replace('id_', '');
+    let firstDataFieldName = labelFieldName === 'part_description' ? 'supplier_part_no' : 'part_description';
+    
     let dotifyText = (text, maxLength) => {
         if (text.length > maxLength) {
            return text.substring(0, maxLength) + '...';
@@ -11,30 +12,34 @@ $('#id_part_description,#id_supplier_part_no').click(function () {
         return text;
      };
 
-    $(`#id_${LabelFieldName}`).autocomplete({
+    $(`#id_${labelFieldName}`).autocomplete({
 
         minLength: 3,
 
         source: (request, response) => {
             let timeStamp = Date.now();
-            $.getJSON(`/order_management/order_autocomplete/${LabelFieldName}=${request.term.trim().replace('=', '')},${timeStamp}`, data => {
+            $.getJSON(`/order_management/order_autocomplete/${labelFieldName}=${request.term.trim().replace('=', '')},${timeStamp}`, data => {
                 response(data);
             })
         },
 
         select: (e, ui) => {
-            Object.entries(ui.item.data).forEach((fieldData) => {
-                $(`#id_${fieldData[0]}`).val(fieldData[1]);
-            });
+            for (let fieldName in ui.item.data)
+                $(`#id_${fieldName}`).val(ui.item.data[fieldName]);
         },
+
     }).autocomplete("instance")._renderItem = (ul, item) => {
-        let spanStyle = 'color:var(--link-fg); margin:0; padding-left:3px; padding-right:3px;';
-        let firstTextElement = LabelFieldName === 'part_description' ? dotifyText(item['label'], 50) : item['label'];
+
+        let firstTextElement = labelFieldName === 'part_description' ? dotifyText(item['label'], 50) : item['label'];
         let secondTextElement = firstDataFieldName === 'part_description' ? dotifyText(item['data'][firstDataFieldName], 50) : item['data'][firstDataFieldName];
+
+        let spanStyle = 'color:var(--link-fg); margin:0; padding-left:3px; padding-right:3px;';
         let displayText = [firstTextElement, secondTextElement, item['data']['supplier'].replace(" GmbH", "")].join(`<span style='${spanStyle}'>┃</span>`);
+        
         return $("<li>")
             .attr("data-value", item.value)
-            .append(`<span style='margin:0px 5px; white-space: nowrap; padding:0;'>${displayText}</span>`)
+            .append(`<span style='margin:0px 5px; white-space: nowrap; padding:3px;'>${displayText}</span>`)
             .appendTo(ul);
+
     }
 });

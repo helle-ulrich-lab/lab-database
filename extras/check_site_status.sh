79d2d8e7a6135768cc3f1e78417206993a9afcc6
Nicola Zilio <nicolazilio@gmail.com>
2022-05-07 18:15:51 +0200

    Rename beyond_django to extras

--- extras/check_site_status.sh
+++ extras/check_site_status.sh
@@ -6,4 +6,4 @@ FILE_DIR="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && /
 DJANGO_PROJECT_DIR=$(echo $FILE_DIR | rev | cut -d'/' -f2- | rev)
 ENV_DIR=$(echo $FILE_DIR | rev | cut -d'/' -f3- | rev)
 
-$ENV_DIR/bin/python $DJANGO_PROJECT_DIR/manage.py shell < $DJANGO_PROJECT_DIR/beyond_django/check_site_status.py
+$ENV_DIR/bin/python $DJANGO_PROJECT_DIR/manage.py shell < $DJANGO_PROJECT_DIR/extras/check_site_status.py

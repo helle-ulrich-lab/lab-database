7223d5a9705b46b6892ad9dbf0da200363e0301a
Nicola Zilio <nicolazilio@gmail.com>
2022-06-23 16:17:31 +0200

    Improved styling for loader

--- static/admin/js/admin/ShowLoading.js
+++ static/admin/js/admin/ShowLoading.js
@@ -6,7 +6,7 @@ function ShowLoading() {
     div.innerHTML = "</br>Please wait...</br></br>";
     div.style.cssText = 'margin: 0; height: 10vw; width: 10vw; position: fixed; \
      top: 50%; left: 50%; z-index: 5000; transform: translate(-50% , -50%); \
-     -webkit-transform: translate(-50%, -50%); box-shadow: 0 0 0 100vmax rgba(0,0,0,.5); \
+     -webkit-transform: translate(-50%, -50%); box-shadow: 0 0 0 100vmax rgba(0, 0, 0, .8); \
      background-color: var(--body-bg); border: 3px solid var(--secondary); \
      border-radius: 3px; font-family: "Roboto"; text-align: center;';
     div.appendChild(loader);

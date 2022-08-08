9727d9dc8b7dc194d00044110f96b9b3e9b26900
Nicola Zilio <nicolazilio@gmail.com>
2022-08-08 11:09:51 +0200

    Add OIDC provider name to login window

---
+++ my_admin/templatetags/settings_value.py
@@ -0,0 +1,14 @@
+# Plainly stolen from https://stackoverflow.com/questions/433162/can-i-access-constants-in-settings-py-from-templates-in-django
+
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+# settings value
+@register.simple_tag
+def settings_value(name):
+    # return getattr(settings, name, "")
+    if name == 'OIDC_PROVIDER_NAME':
+        return getattr(settings, name, "")
+    return ''

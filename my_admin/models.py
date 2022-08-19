985b7520348edcc8d4a7b51aca34488c51df039f
Nicola Zilio <nicolazilio@gmail.com>
2022-08-19 17:27:07 +0200

    Remove join_api_key

--- my_admin/models.py
+++ my_admin/models.py
@@ -5,7 +5,6 @@ class GeneralSetting (models.Model):
     order_email_addresses = models.EmailField("order email address", help_text='Email address to send urgent order notifications', blank=False)
     saveris_username = models.CharField("saveris username", max_length=255, blank=False)
     saveris_password = models.CharField("saveris password", max_length=255, blank=False)
-    join_api_key = models.CharField("join api key", max_length=255, blank=True)
     ms_teams_webhook = models.URLField("MS Teams webhook", max_length=500, blank=True)
 
     class Meta:        
@@ -20,6 +19,5 @@ class GeneralSetting (models.Model):
         # Remove any leading and trailing white spaces
         self.saveris_username = self.saveris_username.strip()
         self.saveris_password = self.saveris_password.strip()
-        self.join_api_key = self.join_api_key.strip()
         
         super(GeneralSetting, self).save(force_insert, force_update, using, update_fields)
